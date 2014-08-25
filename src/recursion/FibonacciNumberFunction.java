d2245f18bfa75c8f6258809acb4e9479a2d18223
Corey Ash <Corey@Sager>
2014-08-25 17:53:29 -0400

    Edit Fibonacci Numbers

--- src/recursion/FibonacciNumberFunction.java
+++ src/recursion/FibonacciNumberFunction.java
@@ -13,19 +13,38 @@ public class FibonacciNumberFunction {
 	 * @param args
 	 */
 	
-	static int Fib(int n)
+	static int Fib(int n) // actually slower than non-recursive
 	{
-		if (n<=0) //base case 1
+		if (n==0) //base case 1
 			return 0;
-		else if (n==0)//base case 2
+		else if (n==1)//base case 2
 			return 1;
 		else
 			return Fib(n-1) + Fib(n-2); //Recursive call
 	}
 	
+	public static int Fib_nr(int n)
+	{
+		int base0 = 0, base1=1;
+		int result = 0;
+		
+		for (int i=2;i<=n;i++)
+		{
+			result = base0 + base1;
+			base0 = base1;
+			base1 = result;
+			
+		}
+		
+		return result;
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Fib(4));
+		int month = 49;
+		System.out.println(Fib(month));
+		System.out.println(Fib_nr(12));
 
 	}
 
