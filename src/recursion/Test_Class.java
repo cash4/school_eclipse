ae5cb350278da0a4302958e0ec1fcac5a54d2495
cash4 <corey.j.ash@gmail.com>
2014-08-25 11:49:04 -0400

    Update Test_Class.java

--- src/recursion/Test_Class.java
+++ src/recursion/Test_Class.java
@@ -4,7 +4,7 @@ public class Test_Class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git is Fun!");
+		System.out.println("Git is Fun!"); //give me more, why is it fun?
 
 	}
 
