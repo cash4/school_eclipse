552f7df3f0f14e563a95d04738928b9e7acca73f
Corey Ash <corey.j.ash@gmail.com>
2014-09-04 20:13:42 -0400

    Finished Assignment 1

--- src/recursion/Assignment_1.java
+++ src/recursion/Assignment_1.java
@@ -107,22 +107,40 @@ public class Assignment_1 {
 		String s3 = "gohangasalamiimalasagnahog";
 		String s4 = "alias";
 		int i1 = 10000;
+		int i2 = 324;
+		int i3 = 5;
+		int i4 = 5643;
 		int gcd1 = 1845729;
 		int gcd2 = 9927189;
+		int gcd3 = 23452;
+		int gcd4 = 6;
+		
+		
 		System.out.println("Is " + s1 + " a palindrome? " + isPalindrome_r(s1));
 		System.out.println("Is " + s2 + " a palindrome? " + isPalindrome_r(s2));
 		System.out.println("Is " + s3 + " a palindrome? " + isPalindrome_r(s3));
 		System.out.println("Is " + s4 + " a palindrome? " + isPalindrome_r(s4));
-		System.out.println("Is " + s1 + " a palindrome? " + isPalindrome_nr(s1));
-		System.out.println("Is " + s2 + " a palindrome? " + isPalindrome_nr(s2));
-		System.out.println("Is " + s3 + " a palindrome? " + isPalindrome_nr(s3));
-		System.out.println("Is " + s4 + " a palindrome? " + isPalindrome_nr(s4));
+		System.out.println("Is " + s1 + " a palindrome? (non-recursive) " + isPalindrome_nr(s1));
+		System.out.println("Is " + s2 + " a palindrome? (non-recursive) " + isPalindrome_nr(s2));
+		System.out.println("Is " + s3 + " a palindrome? (non-recursive) " + isPalindrome_nr(s3));
+		System.out.println("Is " + s4 + " a palindrome? (non-recursive) " + isPalindrome_nr(s4));
 		
-		System.out.println("Sum_r of " + i1 + " is " + sum_r(i1));
-		System.out.println("Sum_nr of " + i1 + " is " + sum_nr(i1));
+		System.out.println("The sum of the harmonic series up to 1/" + i1 + " is " + sum_r(i1));
+		System.out.println("The sum of the harmonic series up to 1/" + i2 + " is " + sum_r(i2));
+		System.out.println("The sum of the harmonic series up to 1/" + i3 + " is " + sum_r(i3));
+		System.out.println("The sum of the harmonic series up to 1/" + i4 + " is " + sum_r(i4));
+		System.out.println("The sum of the harmonic series up to 1/" + i1 + " (non-recursive) is " + sum_nr(i1));
+		System.out.println("The sum of the harmonic series up to 1/" + i2 + " (non-recursive) is " + sum_nr(i2));
+		System.out.println("The sum of the harmonic series up to 1/" + i3 + " (non-recursive) is " + sum_nr(i3));
+		System.out.println("The sum of the harmonic series up to 1/" + i4 + " (non-recursive) is " + sum_nr(i4));
 		
-		System.out.println("GCD of " + gcd1 + " and " + gcd2 + " is: " + getGcd_r(gcd1, gcd2));
-		System.out.println("GCD of " + gcd1 + " and " + gcd2 + " is: " + getGcd_nr(gcd1, gcd2));
+		
+		System.out.println("The greatest common divisor of " + gcd1 + " and " + gcd2 + " is: " + getGcd_r(gcd1, gcd2));
+		System.out.println("The greatest common divisor of " + gcd3 + " and " + gcd2 + " is: " + getGcd_r(gcd3, gcd2));
+		System.out.println("The greatest common divisor of " + gcd4 + " and " + gcd3 + " is: " + getGcd_r(gcd4, gcd3));
+		System.out.println("The greatest common divisor of " + gcd1 + " and " + gcd2 + " (non-recursive) is: " + getGcd_nr(gcd1, gcd2));
+		System.out.println("The greatest common divisor of " + gcd3 + " and " + gcd2 + " (non-recursive) is: " + getGcd_nr(gcd3, gcd2));
+		System.out.println("The greatest common divisor of " + gcd4 + " and " + gcd3 + " (non-recursive) is: " + getGcd_nr(gcd4, gcd3));
 	}
 
 }
